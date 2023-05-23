b03f134e71606c1cd897f7c037738c294bda83c1
Elberte Plinio <elbertedoolan@gmail.com>
2023-05-23 13:14:18 -0300

    identifying user logged

--- lib/app/modules/auth/home/auth_home_page.dart
+++ lib/app/modules/auth/home/auth_home_page.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 
 import '../../../core/ui/extensions/size_screen_extension.dart';
+import '../../../models/user_model.dart';
+import '../../core/auth/auth_store.dart';
 
-class AuthHomePage extends StatelessWidget {
-  const AuthHomePage({super.key});
+class AuthHomePage extends StatefulWidget {
+  final AuthStore _authStore;
+
+  const AuthHomePage({super.key, required AuthStore authStore}) : _authStore = authStore;
+
+  @override
+  State<AuthHomePage> createState() => _AuthHomePageState();
+}
+
+class _AuthHomePageState extends State<AuthHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    reaction<UserModel?>((_) => widget._authStore.userLogged, (userLogged) {
+      if (userLogged != null && userLogged.email.isNotEmpty) {
+        Modular.to.navigate('/home/');
+      } else {
+        Modular.to.navigate('/auth/login/');
+      }
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget._authStore.loadUserLogged();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

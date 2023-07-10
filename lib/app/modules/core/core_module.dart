c3d862ea7e316601becd8699dc656dd63ccc1029
Elberte Plinio <elbertedoolan@gmail.com>
2023-07-10 18:11:13 -0300

    encapsulated dio rest client

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,12 +1,18 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/rest_client/dio/dio_rest_client.dart';
+import '../../core/rest_client/rest_client.dart';
 import 'auth/auth_store.dart';
 
 class CoreModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.lazySingleton(
+        Bind.lazySingleton<AuthStore>(
           (i) => AuthStore(),
+          export: true,
+        ),
+        Bind.lazySingleton<RestClient>(
+          (i) => DioRestClient(),
           export: true,
         ),
       ];

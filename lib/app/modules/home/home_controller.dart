93fb545ddac341c0187dd5de92b59a4e7c749f31
Elberte Plinio <elbertedoolan@gmail.com>
2023-07-12 09:56:00 -0300

    implemented page life cycle

---
+++ lib/app/modules/home/home_controller.dart
@@ -0,0 +1,19 @@
+import 'package:mobx/mobx.dart';
+
+import '../../core/life_cycle/controller_life_cycle.dart';
+part 'home_controller.g.dart';
+
+class HomeController = HomeControllerBase with _$HomeController;
+
+abstract class HomeControllerBase with Store, ControllerLifeCycle {
+  @override
+  void onInit([Map<String, dynamic>? params]) {
+    print('On Init chamado');
+    print(params);
+  }
+
+  @override
+  void onReady() {
+    print('On Ready chamado');
+  }
+}
--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'home_controller.dart';
 import 'home_page.dart';
 
 class HomeModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton(
+          (i) => HomeController(),
+        ),
+      ];
 
   @override
   List<ModularRoute> get routes => [
